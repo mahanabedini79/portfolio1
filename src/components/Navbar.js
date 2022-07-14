8ea0e49bca3f1b790a030d57bb2bb2130503e322
Mahan Abedini <MahanAbediny@yahoo.com>
2022-07-14 17:08:02 +0430

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -102,7 +102,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/mahanabedini79/portfolio"
+                href="https://github.com/mahanabedini79/portfolio1"
                 target="_blank"
                 className="fork-btn-inner"
               >
